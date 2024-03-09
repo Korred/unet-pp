a43fbf64c4dd50263356577554e0039742070a9f
Philipp Kochanski <kochanskip@outlook.com>
2024-03-09 12:24:05 +0100

    chore: Add simple examples on how to train the model and predict masks

--- unetpp/examples.py
+++
@@ -1,7 +0,0 @@
-from model.unetpp import UNetPlusPlus
-
-# Build the model and print the summary
-input_shape = (256, 256, 3)
-num_classes = 1
-model = UNetPlusPlus(input_shape, num_classes).model
-model.summary()
